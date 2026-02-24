2355cd8700b2c5005fbf50e90b2be09efa26a8b6
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2026-02-24 01:18:30 +0100

    new templates

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,5 +1,10 @@
 import blankCode from '../../widget-templates/blank.html?raw';
+import chartCode from '../../widget-templates/chart.html?raw';
+import countdownCode from '../../widget-templates/countdown.html?raw';
+import digitalClockCode from '../../widget-templates/digital-clock.html?raw';
 import statCode from '../../widget-templates/stat.html?raw';
+import visualizerCode from '../../widget-templates/visualizer.html?raw';
+import weatherCode from '../../widget-templates/weather.html?raw';
 
 export interface Template {
   name: string;
@@ -17,4 +22,13 @@ function toTitle(filename: string): string {
 export const templates: Template[] = [
   { name: 'blank', title: toTitle('blank'), code: blankCode },
   { name: 'stat', title: toTitle('stat'), code: statCode },
+  { name: 'weather', title: toTitle('weather'), code: weatherCode },
+  { name: 'chart', title: toTitle('chart'), code: chartCode },
+  { name: 'visualizer', title: toTitle('visualizer'), code: visualizerCode },
+  { name: 'countdown', title: toTitle('countdown'), code: countdownCode },
+  {
+    name: 'digital-clock',
+    title: toTitle('digital-clock'),
+    code: digitalClockCode,
+  },
 ];
